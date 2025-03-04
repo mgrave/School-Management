d92708c584cd6ab1bac77851177e1b66ccf99232
htumba <htumba1105@google.com>
2025-03-04 15:04:34 -0500

    se homologa y se crea el workflow para ci/cd

--- backend/src/middlewares/auth-middleware.js
+++ backend/src/middlewares/auth-middleware.js
@@ -6,6 +6,14 @@ dotenv.config();
 
 const verifyToken = (req, res, next) => {
   console.log("[Auth] Iniciando verificación de token...");
+
+  console.log("[Auth] Request completa:", JSON.stringify(req, null, 2));
+  console.log("[Auth] Headers:", JSON.stringify(req.headers, null, 2));
+  console.log("[Auth] Body:", JSON.stringify(req.body, null, 2));
+  console.log("[Auth] Params:", JSON.stringify(req.params, null, 2));
+  console.log("[Auth] Query:", JSON.stringify(req.query, null, 2));
+  console.log("[Auth] Cookies:", JSON.stringify(req.cookies, null, 2));
+
   const token = req.cookies.Scholarcy;
 
   if (!token) {
