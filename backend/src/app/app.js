bc17180d45e38409937b5f3bc9929bb093cdb2dc
htumba <htumba1105@google.com>
2025-03-10 21:09:10 -0500

    manejo de options y preflight del cors

--- backend/src/app/app.js
+++ backend/src/app/app.js
@@ -22,9 +22,11 @@ export const app = express();
 // Configuración de CORS
 const corsOptions = {
   origin: [process.env.ORIGIN, "http://localhost:3000", "http://localhost:5173", "https://schoolfrontend.vercel.app/"],
-  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE", 'OPTIONS'],
   credentials: true,
 };
+
+app.options('*', cors()); // Manejar solicitudes OPTIONS
 
 // Middleware para registrar el resultado de CORS
 app.use((req, res, next) => {
